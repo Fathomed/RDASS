8dce62147ff0d3020c259ed57a2e8ee6e02e457f
Jeff Strang <jeff@fathomsolutions.co.nz>
2021-12-22 16:04:22 +1300

    commit changes

--- quiz.js
+++ quiz.js
@@ -6,6 +6,8 @@ const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressbarFull");
 const loader = document.getElementById("loader");
 const game = document.getElementById("game");
+const referencePage = document.getElementById("reference-page");
+const referenceLink = document.getElementById("reference-link");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -14,79 +16,81 @@ let questionCounter = 0;
 const availableQuestions = [];
 
 let questions = [];
-fetch("https://fathomed.github.io/survivethedive-quiz/questions.json")
-  .then((res) => res.json())
-  .then((loadedQuestions) => {
-    console.log(loadedQuestions);
-    questions = loadedQuestions;
-    startGame();
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+fetch("https://fathomed.github.io/rdass/questions.json")
+	.then((res) => res.json())
+	.then((loadedQuestions) => {
+		console.log(loadedQuestions);
+		questions = loadedQuestions;
+		startGame();
+	})
+	.catch((err) => {
+		console.error(err);
+	});
 
 // CONSTANTS
 const CORRECT_BONUS = 1;
 const MAX_QUESTIONS = 10;
 
 startGame = () => {
-  questionCounter = 0;
-  score = 0;
-  availableQuesions = [...questions];
-  getNewQuestion();
-  game.classList.remove("hidden");
-  loader.classList.add("hidden");
+	questionCounter = 0;
+	score = 0;
+	availableQuesions = [...questions];
+	getNewQuestion();
+	game.classList.remove("hidden");
+	loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
-  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score);
-    // go to the end page
-    return window.location.assign("/quiz-pages/quiz-end");
-  }
-  questionCounter++;
-  progressText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
-  // Update the progress bar
-  progressbarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+	if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+		localStorage.setItem("mostRecentScore", score);
+		// go to the end page
+		return window.location.assign("/quiz-pages/quiz-end");
+	}
+	questionCounter++;
+	progressText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+	// Update the progress bar
+	progressbarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-  currentQuestion = availableQuesions[questionIndex];
-  header.innerText = currentQuestion.header;
-  question.innerText = currentQuestion.question;
+	const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+	currentQuestion = availableQuesions[questionIndex];
+	header.innerText = currentQuestion.header;
+	question.innerText = currentQuestion.question;
+	referencePage.innerText = currentQuestion.referencepage;
+	referenceLink.innerHTML = currentQuestion.referencelink;
 
-  choices.forEach((choice) => {
-    const { number } = choice.dataset;
-    choice.innerText = currentQuestion[`choice${number}`];
-  });
+	choices.forEach((choice) => {
+		const { number } = choice.dataset;
+		choice.innerText = currentQuestion[`choice${number}`];
+	});
 
-  availableQuesions.splice(questionIndex, 1);
-  acceptingAnswers = true;
+	availableQuesions.splice(questionIndex, 1);
+	acceptingAnswers = true;
 };
 
 choices.forEach((choice) => {
-  choice.addEventListener("click", (e) => {
-    if (!acceptingAnswers) return;
+	choice.addEventListener("click", (e) => {
+		if (!acceptingAnswers) return;
 
-    acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset.number;
+		acceptingAnswers = false;
+		const selectedChoice = e.target;
+		const selectedAnswer = selectedChoice.dataset.number;
 
-    const classToApply =
-      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+		const classToApply =
+			selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-    if (classToApply === "correct") {
-      incrementScore(CORRECT_BONUS);
-    }
-    selectedChoice.parentElement.classList.add(classToApply);
+		if (classToApply === "correct") {
+			incrementScore(CORRECT_BONUS);
+		}
+		selectedChoice.parentElement.classList.add(classToApply);
 
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(classToApply);
-      getNewQuestion();
-    }, 1000);
-  });
+		setTimeout(() => {
+			selectedChoice.parentElement.classList.remove(classToApply);
+			getNewQuestion();
+		}, 1000);
+	});
 });
 
 incrementScore = (num) => {
-  score += num;
-  scoreText.innerText = score;
+	score += num;
+	scoreText.innerText = score;
 };
